b7295f1e6310391fa923c81c4f7c0b956df82e48
RudyPrzyjaciel <szynks20@gmail.com>
2022-09-08 11:26:10 +0200

    Simulating the hailstone sequence

---
+++ hailstone_sequence_challenge.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int value = 1;
+    int count = 1;
+
+    printf("Enter starting integer: ");
+    scanf("%d", &value);
+    
+    printf("Hailstone sequence:\n   %d", value);
+    while (value != 1)
+    {
+        value = (value % 2 == 1) ? (value = value * 3 + 1) : (value = value / 2);
+        printf(", %d", value);
+    }
+    printf("\nSequence length: %d\n", count);
+    
+    return 0;
+}
